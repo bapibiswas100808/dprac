0d6556d86d443b08901a6787c762f5a107250ad9
bapibiswas100808 <star.bappy@gmail.com>
2023-07-25 14:39:44 +0600

    usecontext done

--- src/App.js
+++ src/App.js
@@ -6,19 +6,34 @@ import Footer from "./Components/Footer/Footer";
 import Registration from "./Components/Registration/Registration";
 import Login from "./Components/Login/Login";
 import { Route, Routes } from "react-router-dom";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Registration />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/home" element={<Home></Home>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/home" element={<Home></Home>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <section className="switch">
+          <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </section>
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
